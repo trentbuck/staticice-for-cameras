0c158a05c0b63201bd8236ec886fb2f9d41660b6
Trent W. Buck <trentbuck@gmail.com>
2020-09-23 23:40:28 +1000

    change the scraper to walk "next page" and extract raw quote fields

--- test2/test2/spiders/quotes.py
+++ test2/test2/spiders/quotes.py
@@ -1,15 +1,31 @@
 import scrapy
+import typing
 import pathlib
 
+# FIXME: these don't error:
+#   scrapy shell https://example.com/LKJASLDKASJLKDJASD-DOES-NOT-EXIST
+#   scrapy shell https://expired.badssl.com/
 
 class X(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['https://quotes.toscrape.com/page/1/']
+    start_urls = ['http://quotes.toscrape.com/']
 
-    def parse(self, response):
-        page = response.url.split('/')[-2]
-        path = pathlib.Path(f'quotes-{page}.html')
-        with path.open('w') as f:
-            print(response.body, end='', file=f)
-        self.log(f'Saved file {path}')
+    # Convert the "GET https://quotes.toscrape.com/page/N/"
+    # into a list of items (quotes).
+    def parse(self, response: scrapy.http.response.html.HtmlResponse) -> typing.Iterator[dict]:
+        # FIXME: when xpath has <1 match, .get() cheerfully returns None.
+        # FIXME: when xpath has >1 match, .get() cheerfully returns the first.
+        # How do I get exceptions for both cases?
+        for quote_etree in response.xpath('//*[@itemscope]'):
+            yield {
+                'author': quote_etree.xpath('.//*[@itemprop="author"]/text()').get(),
+                'text': quote_etree.xpath('.//*[@itemprop="text"]/text()').get(),
+                'tags': quote_etree.xpath('.//*[@class="tag"]/text()').getall()}
+
+        # Recursively descend the next page.
+        # Follow the "next page" link
+        for next_url in response.xpath('//li[@class="next"]/a/@href').getall():
+            yield scrapy.Request(
+                response.urljoin(next_url),
+                callback=self.parse)
