efb8b9fc9c6e06cd7a3b2ec9e7c1ba1ac58a3d76
Trent W. Buck <trentbuck@gmail.com>
2020-09-23 21:34:46 +1000

    tutorial first commit

--- test2/test2/spiders/quotes.py
+++ test2/test2/spiders/quotes.py
@@ -1,10 +1,15 @@
 import scrapy
+import pathlib
 
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    start_urls = ['https://quotes.toscrape.com/page/1/']
 
     def parse(self, response):
-        pass
+        page = response.url.split('/')[-2]
+        path = pathlib.Path(f'quotes-{page}.html')
+        with path.open('w') as f:
+            print(response.body, end='', file=f)
+        self.log(f'Saved file {path}')
