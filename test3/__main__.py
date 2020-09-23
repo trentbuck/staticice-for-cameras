3bd89c7ef724821d5011f90e740c49955d7314b2
Trent W. Buck <trentbuck@gmail.com>
2020-09-24 01:22:26 +1000

    fixup! Bare minimum for a zipapp scraper

--- test3/__main__.py
+++ test3/__main__.py
@@ -6,6 +6,8 @@ import test3.spider
 #   a "project" is a directory containing scrapy.cfg and a "crawler".
 #   a "crawler" is a python module that contains multiple "spiders" (& optionally more)
 #   a "spider"  is a python module that does the ACTUAL SCRAPING.
+#   an "item"   is a python object representing one scraped "thing" - e.g. on asos.com, a dress.
+
 
 proc = scrapy.crawler.CrawlerProcess()
 proc.crawl(crawler_or_spidercls=test3.spider.X)
